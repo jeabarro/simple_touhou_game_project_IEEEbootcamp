66ea18b19ee21539b69db2457d24ef70e81a9893
jeabarro <jeabarro1@up.edu.ph>
2026-09-19 16:31:39 +0800

    Update hvsync_generator.v

--- src/hvsync_generator.v
+++ src/hvsync_generator.v
@@ -20,14 +20,14 @@ module hvsync_generator(clk, reset, hsync, vsync, display_on, hpos, vpos);
 
   // declarations for TV-simulator sync parameters
   // horizontal constants
-  parameter H_DISPLAY       = 640; // horizontal display width
-  parameter H_BACK          =  48; // horizontal left border (back porch)
-  parameter H_FRONT         =  16; // horizontal right border (front porch)
-  parameter H_SYNC          =  96; // horizontal sync width
+  parameter H_DISPLAY       = 320; // horizontal display width
+  parameter H_BACK          =  24; // horizontal left border (back porch)
+  parameter H_FRONT         =  8; // horizontal right border (front porch)
+  parameter H_SYNC          =  48; // horizontal sync width
   // vertical constants
-  parameter V_DISPLAY       = 480; // vertical display height
-  parameter V_TOP           =  33; // vertical top border
-  parameter V_BOTTOM        =  10; // vertical bottom border
+  parameter V_DISPLAY       = 240; // vertical display height
+  parameter V_TOP           =  16; // vertical top border
+  parameter V_BOTTOM        =  5; // vertical bottom border
   parameter V_SYNC          =   2; // vertical sync # lines
   // derived constants
   parameter H_SYNC_START    = H_DISPLAY + H_FRONT;
